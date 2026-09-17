6cce9e7d82c99c581a2369f792c10353695f446b
leo028-SC <leonardo.sc274@gmail.com>
2026-09-17 17:18:06 -0600

    Trabajamos con PHP  y lo basico en  HTML

---
+++ index.php
@@ -0,0 +1,35 @@
+<!--  <?php
+    echo "Hola, Mundo!";
+?>-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1{
+            color: #53284f;
+        }
+        div{
+            /* Fondo */
+            background : #b590d0;
+
+        }
+    </style>
+</head>
+<body>
+    <h1>Holoooooooo </h1>
+    <h2>Holoooooooo</h2>
+    <h3>Holoooooooo</h3>
+    <h4>Holoooooooo</h4>
+    <h5>Holoooooooo</h5>
+    <h6>Holoooooooo</h6>
+    
+    <div>
+        <h2>Título del párrafo</h2>
+
+        <p>Este es un párrafo</p>
+    </div>
+</body>
+</html>
