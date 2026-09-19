8666395876f46ce37c372be7489ddae149dc3817
leo028-SC <leonardo.sc274@gmail.com>
2026-09-18 18:49:05 -0600

    Introduccion a las css y box model

--- index.php
+++ index.php
@@ -11,11 +11,108 @@
         h1{
             color: #53284f;
         }
-        div{
+        .container1{
             /* Fondo */
             background : #b590d0;
 
+            padding: 40px 20px;
+            margin: 20px;
+            border: 3px solid black;
+
         }
+        .container2{
+            /* Fondo */
+            background : #d76576;
+
+            padding-top: 40px; 
+            padding-right: 20px;
+            padding-bottom: 40px; 
+            padding-left: 20px; 
+            margin-top: 20px;
+            margin-right: 30px;
+            margin-bottom: 40px;
+            margin-left: 50px;
+        }
+.solid {
+    background-color: darkblue;
+    color: white;
+    padding: 20px;
+    margin: 20px;
+    border: 5px solid black;
+    }
+
+.dashed {
+    background-color: darkgreen;
+    color: white;
+    padding: 20px;
+    margin: 20px;
+    border: 5px dashed black;
+}
+
+.dotted {
+    background-color: darkred;
+    color: white;
+    padding: 20px;
+    margin: 20px;
+    border: 5px dotted black;
+}
+
+.double {
+    background-color: darkgray;
+    color: white;
+    padding: 20px;
+    margin: 20px;
+    border: 5px double black;
+}
+
+.groove {
+    background-color: darkblue;
+    color: white;
+    padding: 20px;
+    margin: 20px;
+    border: 5px groove black;
+}
+
+.ridge {
+    background-color: darkgreen;
+    color: white;
+    padding: 20px;
+    margin: 20px;
+    border: 5px ridge black;
+}
+
+.inset {
+    background-color: darkred;
+    color: white;
+    padding: 20px;
+    margin: 20px;
+    border: 5px inset black;
+}
+
+.outset {
+    background-color: darkgray;
+    color: white;
+    padding: 20px;
+    margin: 20px;
+    border: 5px outset black;
+}
+
+.none {
+    background-color: darkblue;
+    color: white;
+    padding: 20px;
+    margin: 20px;
+    border: 5px none black;
+}
+
+.hidden {
+    background-color: darkgreen;
+    color: white;
+    padding: 20px;
+    margin: 20px;
+    border: 5px hidden black;
+}
+
     </style>
 </head>
 <body>
@@ -26,10 +123,79 @@
     <h5>Holoooooooo</h5>
     <h6>Holoooooooo</h6>
     
-    <div>
+    <div class="container1">
         <h2>Título del párrafo</h2>
 
-        <p>Este es un párrafo</p>
+        <p>lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, 
+            voluptatum. Lorem ipsum dolor sit amet
+        </p>
     </div>
+    <div class="container2">
+        <h2>Título del párrafo</h2>
+
+        <p>lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, 
+            voluptatum. Lorem ipsum dolor sit amet
+        </p>
+    </div>
+
+    <div class="solid">
+        <h2>Solid</h2>
+        <p>Ejemplo de borde solid.</p>
+    </div>
+
+
+    <div class="dashed">
+        <h2>Dashed</h2>
+        <p>Ejemplo de borde dashed.</p>
+    </div>
+
+    <div class="dotted">
+        <h2>Dotted</h2>
+        <p>Ejemplo de borde dotted.</p>
+    </div>
+
+
+    <div class="double">
+        <h2>Double</h2>
+        <p>Ejemplo de borde double.</p>
+    </div>
+
+
+    <div class="groove">
+        <h2>Groove</h2>
+        <p>Ejemplo de borde groove.</p>
+    </div>
+
+
+    <div class="ridge">
+        <h2>Ridge</h2>
+        <p>Ejemplo de borde ridge.</p>
+    </div>
+
+
+    <div class="inset">
+        <h2>Inset</h2>
+        <p>Ejemplo de borde inset.</p>
+    </div>
+
+
+    <div class="outset">
+        <h2>Outset</h2>
+        <p>Ejemplo de borde outset.</p>
+    </div>
+
+
+    <div class="none">
+        <h2>None</h2>
+        <p>Ejemplo de borde none.</p>
+    </div>
+
+
+    <div class="hidden">
+        <h2>Hidden</h2>
+        <p>Ejemplo de borde hidden.</p>
+    </div>
+
+
 </body>
 </html>
